17f2b3c5c994e392316406b684226f52e3380fe3
vrojas <vrojas@fazenda.ms.gov.br>
2024-04-04 11:03:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.cookie('strictCookie', 'someValue', {
+        httpOnly: true,
+        sameSite: 'Strict',
+        path: '/',
+        secure: true // Secure should be true if using https
+    });
+    res.send('Cookie has been set with SameSite=Strict');
+});
+
+app.get('/protected', (req, res) => {
+    if (req.cookies.strictCookie === 'someValue') {
+        res.send('Dados protegidos exibidos');
+    } else {
+        res.status(401).send('Acesso Negado, você precisa do cookie');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
