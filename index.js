b3a3c7bcaec55391e591081c9383dd69724c5c87
vrojas <vrojas@fazenda.ms.gov.br>
2024-04-04 16:28:46 -0300

    modificando porta

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const app = express();
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
+
+    res.send('try https://teste-cookie-strict-ygn7.vercel.app/set-cookie to generate the cookie and https://teste-cookie-strict-ygn7.vercel.app/protected to test the cookie permission');
+});
+
+app.get('/set-cookie', (req, res) => {
     res.cookie('strictCookie', 'someValue', {
         httpOnly: true,
         sameSite: 'Lax',
         path: '/',
         secure: true // Secure should be true if using https
     });
-    res.send('Cookie has been set with SameSite=Strict');
 });
 
 app.get('/protected', (req, res) => {
